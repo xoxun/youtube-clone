b1de5831ea843038771ca9e7c688a54b1836c819
xoxun <00xxs2@naver.com>
2023-08-23 22:46:49 +0900

    feat: Add middleware

--- src/server.js
+++ src/server.js
@@ -1,15 +1,52 @@
 import express from "express";
+import morgan from "morgan"; //미들웨어 설치해 사용하기
 
 const PORT = 7777;
+const date = new Date();
 
 const app = express(); //App 생성!
+const logger = morgan("dev"); //
 
 //App 설정 ------
 
-app.get("/", (req, res) => res.send("<h1>Home</h1>"));
-app.get("/about", (req, res) => res.send("<h1>About</h1>"));
-app.get("/contact", (req, res) => res.send("<h1>Contact</h1>"));
-app.get("/login", (req, res) => res.send("<h1>Login</h1>"));
+
+const urlLogger = (req,res,next)=>{
+  console.log(`Path: ${req.path}`);
+  next();
+
+};
+const timeLogger = (req,res,next)=>{
+  console.log(`Time: ${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}`);
+  next();
+};
+
+const securityLogger = (req,res,next)=>{
+  const protocol = req.protocol;
+  if(protocol ==='https'){
+    console.log("Secure");
+  }else{
+    console.log("Insecure");
+  }
+  next();
+};
+const protectorMiddleware = (req,res,next)=>{
+  const url = req.url;
+  if(url==="/protected"){
+    return res.send("<h1>Not Allowed!✖️</h1>");
+  }
+  next();
+};
+
+const home = (req,res)=>{
+  return res.send("<h1>Home</h1>");
+};
+const privateHome = (req,res)=>{
+  return res.send("<h1>Private Home</h1>");
+}
+
+app.use(urlLogger,timeLogger,securityLogger,protectorMiddleware);
+app.get("/",home);
+app.get("/protected",privateHome);
 
 //App 보여주기 ! ------
 const handleListening = () =>
